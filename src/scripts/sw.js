cc6c615f7022ec548c4bd3842e43dd037674c1ec
Muhammad Fadhilah Hanan <88243095+Vareniel@users.noreply.github.com>
2024-05-24 07:21:27 +0700

    Add files via upload

---
+++ src/scripts/sw.js
@@ -0,0 +1,32 @@
+import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute, Route } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+
+// Do precaching
+precacheAndRoute(self.__WB_MANIFEST);
+
+const restaurantdbApi = new Route(
+  ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev/'),
+  new StaleWhileRevalidate({
+    cacheName: 'restaurantdb-api',
+  }),
+);
+
+const restaurantdbImageApi = new Route(
+  ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev/images/medium/'),
+  new StaleWhileRevalidate({
+    cacheName: 'restaurantdb-image-api',
+  }),
+);
+
+registerRoute(restaurantdbApi);
+registerRoute(restaurantdbImageApi);
+
+self.addEventListener('install', () => {
+  console.log('Service Worker: Installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('push', () => {
+  console.log('Service Worker: Pushed');
+});
---
+++ src/scripts/utils/refresh.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-cycle */
+/* eslint-disable no-undef */
+import Detail from '../views/pages/detail';
+
+const renderDetailPage = async () => {
+  const mainContent = document.querySelector('resto-detail');
+  mainContent.innerHTML = await Detail.render();
+  await Detail.afterRender();
+
+  window.location.reload();
+};
+
+export default renderDetailPage;
